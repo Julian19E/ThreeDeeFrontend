b3879a56d4c702635adfce57a72c433ac7ff1aab
gabrielmandtler <g.mandtler@gmx.de>
2023-03-07 17:16:08 +0100

    Create Page for Sharedfiles with css

---
+++ ThreeDeeFrontend/Pages/SharedFile.razor.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using ThreeDeeApplication.Enums;
+using ThreeDeeApplication.Models;
+using ThreeDeeInfrastructure.Repositories;
+
+namespace ThreeDeeFrontend.Pages;
+
+public partial class SharedFile
+{
+    [Inject]
+    private IFileRepository FileRepository { get; set; }
+    
+    [Inject]
+    private NavigationManager NavigationManager { get; set; }
+
+    private List<FileModel> _filteredSharedFiles = new();
+
+    protected override void OnParametersSet()
+     {
+         foreach (FileModel model in FileRepository.MockData)
+        {
+            if (model.Filetype == Filetype.Shared)
+            {
+                _filteredSharedFiles.Add(model);
+            }
+        }
+     }
+
+    private void OnButtonClicked(int fileId)
+    {
+        NavigationManager.NavigateTo($"/model/{fileId}");
+    }
+
+    private async Task OnFilteredValueChanged(List<FileModel> filtered)
+    {
+        _filteredSharedFiles = filtered;
+        await InvokeAsync(StateHasChanged);
+    }
+}
